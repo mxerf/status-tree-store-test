194a78006df944a6981e9cd7514bc03d3951d7ae
MXerFix <maksim2003003@gmail.com>
2025-03-17 17:25:10 +0300

    init: setup Nuxt 3, TypeScript, AgGrid, and project structure

---
+++ app.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts"></script>
+
+<template>
+  <NuxtPage />
+</template>
+
+<style>
+@import "@ag-grid-community/styles/ag-grid.css";
+@import "@ag-grid-community/styles/ag-theme-alpine.css";
+</style>
